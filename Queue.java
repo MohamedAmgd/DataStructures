31dd3e22a7e89f7099c3dd94de4001e8c39afa4c
Mohamed Amgd <megapof2@gmail.com>
2020-04-12 10:38:55 +0200

    finished implementing queue

--- Queue.java
+++ Queue.java
@@ -3,12 +3,18 @@
  */
 public class Queue<T> extends LinkedList<T> {
 
-    public void push() {
-
+    public Queue() {
     }
 
-    public void pop() {
-        deleteByPos(0);
+    public Queue(T... values) {
+        super(values);
     }
 
+    public void enqueue(T... values) {
+        insert(values);
+    }
+
+    public T dequeue() {
+        return deleteByPos(0);
+    }
 }
